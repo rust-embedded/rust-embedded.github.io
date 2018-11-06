rust-embedded/book

commit cf66108f200f6b6eb75d47fdaf72f05684060a06

ignore print.html
to avoid errors caused by bug rust-lang-nursery/mdBook#789

ci/script.sh
@@ -3,13 +3,15 @@ set -euxo pipefail
 main() {
     mdbook build
 
-    linkchecker book
+    # FIXME(rust-lang-nursery/mdbook#789) remove `--ignore-url` when that bug is fixed
+    linkchecker --ignore-url "print.html" book
 
     # now check this as a directory of the bookshelf
     rm -rf shelf
     mkdir shelf
     mv book shelf
-    linkchecker shelf
+    # FIXME(rust-lang-nursery/mdbook#789) remove `--ignore-url` when that bug is fixed
+    linkchecker --ignore-url "print.html" shelf
 
     mv shelf/book .
     rmdir shelf

rust-embedded/rust-embedded.github.io

commit 1a8a7d75fa1e65f0e8ca4fb2c8cd3ac9c845ee58

Ignore all print.html files from linkchecker
Previously I was just ignoring the print.html for the discovery book
since it was the only bookshelf documentation that was failing link
checks in print.html but the embedded Rust book is failing too now
after
 https://github.com/rust-embedded/book/commit/cf66108f200f6b6eb75d47fdaf72f05684060a06
and
 https://github.com/rust-embedded/book/commit/e310669d0e8ecd0309f22321378b84857db15f22

ci/script.sh
@@ -49,8 +49,8 @@ main() {
     rm -rf $tmpdir
 
     # check links
-    # mdbook doesn't handle relative links correctly in print.html so skip it.
-    linkchecker --ignore-url "discovery/print.html" doc
+    # FIXME(rust-lang-nursery/mdbook#789) remove `--ignore-url` when that bug is fixed
+    linkchecker --ignore-url "print.html" doc
 }
 
 main